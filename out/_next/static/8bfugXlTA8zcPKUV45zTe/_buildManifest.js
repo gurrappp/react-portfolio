94587d8e29f7ed6af891e6eaad5aee42d777c206
Gustav Polhem <gustavpolhem@gmail.com>
2024-05-30 22:40:55 +0200

    test

---
+++ out/_next/static/8bfugXlTA8zcPKUV45zTe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/2e3a845b-55d6e1ac8f1c8161.js","static/chunks/923-9a917a482414b216.js","static/chunks/pages/index-01ad054121d41b54.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
