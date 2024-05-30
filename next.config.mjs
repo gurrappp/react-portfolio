072bcf4d55affbde929e5878f98541021a036704
Gustav Polhem <gustavpolhem@gmail.com>
2024-05-30 22:10:13 +0200

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "/react-portfolio",
   output: "export",  // <=== enables static exports
-  webpack: (config, options) => {
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle PDF files
     config.module.rules.push({
       test: /\.(pdf)$/,
-      type: "asset/source"
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
     });
+
+    if (!isServer) {
+      // Ensure that the file-loader is applied to the client as well
+      config.resolve.fallback.fileSystem = false;
+    }
+
     return config;
   },
 };
